d9e0989f56732346187ed2c9d4470e50665c9b9f
Zea-Zee <glebru.irk38@yandex.ru>
2023-12-10 14:47:26 +0700

    25th Lab

--- g.kuznetsov2/Lab25/c.c
+++ g.kuznetsov2/Lab25/c.c
@@ -3,56 +3,71 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <signal.h>
 #include <ctype.h>
+#include <termios.h>
+
+
+//Change termios settings to not display user entered text and adjust recover default settings at exit
+struct termios original_termios;
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enable_raw_mode() {
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disable_raw_mode);
+
+    struct termios raw = original_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
-    //create program channel
+    enable_raw_mode();
+
     int pipe_fd[2];
+    pid_t child_pid;
+
     if (pipe(pipe_fd) == -1) {
-        perror("Pipe creation failed");
+        perror("pipe");
         exit(1);
     }
 
-    pid_t pid = fork();
-    if (pid == -1) {
-        perror("Fork failed");
+    if ((child_pid = fork()) == -1) {
+        perror("fork");
         exit(2);
     }
 
-    if (pid == 0) {
-        //close write df
+    if (child_pid == 0) {
         close(pipe_fd[1]);
-        char buffer[100];
-        ssize_t bytes_read;
-        //read from channel
-        while ((bytes_read = read(pipe_fd[0], buffer, sizeof(buffer))) > 0) {
-            for (ssize_t i = 0; i < bytes_read; ++i) {
-                buffer[i] = toupper(buffer[i]);
+        signal(SIGQUIT, exit);
+
+        char buffer;
+        while (read(pipe_fd[0], &buffer, 1) > 0) {
+            if (buffer == '\0') {
+                break;
             }
-            write(STDOUT_FILENO, buffer, bytes_read);
+            char uppercase = toupper(buffer);
+            write(STDOUT_FILENO, &uppercase, 1);
         }
 
-        // Закрываем дескриптор чтения
         close(pipe_fd[0]);
-        printf("Child process finished.\n");
+        printf("Child process finished\n");
+//        exit(EXIT_SUCCESS);
     } else {
-        // Родительский процесс (писатель)
-
-        // Закрываем дескриптор чтения
         close(pipe_fd[0]);
+        signal(SIGQUIT, exit);
 
-        const char* message = "Hello, World!";
-
-        // Запись в канал
-        write(pipe_fd[1], message, strlen(message));
-
-        // Закрываем дескриптор записи
-        close(pipe_fd[1]);
-
-        // Ожидание завершения дочернего процесса
-        wait(NULL);
-        printf("Parent process finished.\n");
+        char input;
+        while (read(STDIN_FILENO, &input, 1) > 0) {
+            write(pipe_fd[1], &input, 1);
+        }
+//
+//        close(pipe_fd[1]);
+//        kill(child_pid, SIGQUIT);
+//        wait(NULL);
+//        exit(EXIT_SUCCESS);
     }
-
-    return 0;
 }
